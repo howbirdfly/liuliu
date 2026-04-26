319e8f16ff5daae00842acba0d4fa2e95a66df3e
howbirdfly <1132768287@qq.com>
2026-04-26 15:12:08 +0800

    删掉前端没用的配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-
-  return {
-    plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      allowedHosts: ['7c6c7ed5.r8.cpolar.top', '78e189b3.r8.cpolar.top'],
-    },
-  };
+  },
+  server: {
+    hmr: process.env.DISABLE_HMR !== 'true',
+    allowedHosts: ['7c6c7ed5.r8.cpolar.top', '78e189b3.r8.cpolar.top'],
+  },
 });
